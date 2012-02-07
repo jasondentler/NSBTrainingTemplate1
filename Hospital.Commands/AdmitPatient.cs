83cc8da1e9d9b6b64b009a93f158f52346b3a00a
unknown <jdentler@DWH-HSTJDENTLER.corp.weekleyhomes.com>
2012-02-07 13:43:19 -0600

    Solution structure.

---
+++ Hospital.Commands/AdmitPatient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Hospital.Commands
+{
+    public class AdmitPatient  
+    {
+
+        public Guid PatientId { get; set; }
+
+    }
+}
---
+++ Hospital.Events/PatientAdmitted.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Hospital.Events
+{
+    public class PatientAdmitted  
+    {
+
+        public Guid PatientId { get; set; }
+
+    }
+}
