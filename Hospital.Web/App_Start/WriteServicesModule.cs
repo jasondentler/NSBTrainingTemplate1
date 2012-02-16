ec5ce96e670bfedfb7811c57201e4efdad04a18f
Jason Dentler <jasondentler@gmail.com>
2012-02-16 07:25:32 -0600

    Backing out exercise12 -> exercise13 changes

--- Hospital.Web/App_Start/WriteServicesModule.cs
+++ Hospital.Web/App_Start/WriteServicesModule.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using NServiceBus;
-using NServiceBus.ObjectBuilder.Ninject.Config;
-using Hospital.WriteModel;
+﻿using Hospital.WriteModel;
 using Ninject.Modules;
 
 namespace Hospital.Web.App_Start
@@ -13,22 +10,6 @@ namespace Hospital.Web.App_Start
             Kernel.Bind<IHospitalWriteService>()
                 .To<HospitalWriteService>()
                 .InSingletonScope();
-
-            Func<Type, bool> isCommand = t => t.IsAssignableFrom(typeof(ICommand));
-            Func<Type, bool> isEvent = t => t.IsAssignableFrom(typeof(IEvent));
-            Func<Type, bool> isMessage = t => t.IsAssignableFrom(typeof(IMessage));
-            
-            Configure.WithWeb()
-                .DefineEndpointName(typeof (MvcApplication).Namespace)
-                .NinjectBuilder(Kernel)
-                .DefiningMessagesAs(isMessage)
-                .DefiningCommandsAs(isCommand)
-                .DefiningEventsAs(isEvent)
-                .Log4Net()
-                .JsonSerializer()
-                .MsmqTransport()
-                .UnicastBus()
-                .SendOnly();
         }
     }
 }
--- Hospital.WriteModel/HospitalWriteService.cs
+++ Hospital.WriteModel/HospitalWriteService.cs
@@ -1,61 +1,24 @@
 ﻿using System;
-using Hospital.Commands;
-using NServiceBus;
 
 namespace Hospital.WriteModel
 {
     public class HospitalWriteService : IHospitalWriteService
     {
-        private readonly IBus _bus;
-
-        public HospitalWriteService(IBus bus)
-        {
-            _bus = bus;
-        }
 
         public void CreatePatient(Guid patientId, string firstName, string lastName)
         {
-            var cmd = new CreatePatient()
-                          {
-                              CommandId = Guid.NewGuid(),
-                              PatientId = patientId,
-                              FirstName = firstName,
-                              LastName = lastName
-                          };
-            _bus.Send(cmd);
         }
 
         public void AdmitPatient(Guid patientId, DateTimeOffset when)
         {
-            var cmd = new AdmitPatient()
-                          {
-                              CommandId = Guid.NewGuid(),
-                              PatientId = patientId,
-                              When = when
-                          };
-            _bus.Send(cmd);
         }
 
         public void AssignBed(Guid patientId, int bed)
         {
-            var cmd = new AssignBed()
-                          {
-                              CommandId = Guid.NewGuid(),
-                              PatientId = patientId,
-                              Bed = bed
-                          };
-            _bus.Send(cmd);
         }
 
         public void DischargePatient(Guid patientId, DateTimeOffset when)
         {
-            var cmd = new DischargePatient()
-                          {
-                              CommandId = Guid.NewGuid(),
-                              PatientId = patientId,
-                              When = when
-                          };
-            _bus.Send(cmd);
         }
     }
 }
