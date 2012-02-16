a9045c0e74aa18fe18f9ed76cb42c22e57f45c0c
Jason Dentler <jasondentler@gmail.com>
2012-02-16 08:16:03 -0600

    Added name to PatientCreated and added reference to core

--- Hospital.Events/PatientCreated.cs
+++ Hospital.Events/PatientCreated.cs
@@ -7,6 +7,8 @@ namespace Hospital.Events
 
         public Guid EventId { get; set; }
         public Guid PatientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
     }
 }
