bc451f91ca76a2c3aaa7632383063b587a4fdc54
Jason Dentler <jasondentler@gmail.com>
2012-02-16 08:06:00 -0600

    Work on exercise13 -> exercise14

---
+++ Hospital.CommandHandlers/AggregateFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CommonDomain;
+using CommonDomain.Persistence;
+using Ninject;
+using Ninject.Parameters;
+
+namespace Hospital.CommandHandlers
+{
+    public class AggregateFactory : IConstructAggregates 
+    {
+        private readonly IKernel _kernel;
+
+        public AggregateFactory(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public IAggregate Build(Type type, Guid id, IMemento snapshot)
+        {
+            return (IAggregate) _kernel.Get(type, new ConstructorArgument("id", id));
+        }
+    }
+}
---
+++ Hospital.Domain/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CommonDomain.Core;
+using Hospital.Events;
+
+namespace Hospital.Domain
+{
+    public class Patient : AggregateBase 
+    {
+
+        public Patient(Guid id)
+        {
+            Id = id;
+        }
+
+        public Patient(Guid id, string firstName, string lastName) : this(id)
+        {
+            RaiseEvent(new PatientCreated()
+                           {
+                               EventId = Guid.NewGuid(),
+                               PatientId = Id
+                           });
+
+        }
+
+    }
+}
