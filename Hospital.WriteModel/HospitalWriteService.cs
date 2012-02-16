0e342c5f29c799811a2e9fc5322afcc58c35af4e
Jason Dentler <jasondentler@gmail.com>
2012-02-15 20:30:51 -0600

    Hospital.WriteModel now sends commands

--- Hospital.WriteModel/HospitalWriteService.cs
+++ Hospital.WriteModel/HospitalWriteService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Hospital.Commands;
 using NServiceBus;
 
 namespace Hospital.WriteModel
@@ -14,18 +15,47 @@ namespace Hospital.WriteModel
 
         public void CreatePatient(Guid patientId, string firstName, string lastName)
         {
+            var cmd = new CreatePatient()
+                          {
+                              CommandId = Guid.NewGuid(),
+                              PatientId = patientId,
+                              FirstName = firstName,
+                              LastName = lastName
+                          };
+            _bus.Send(cmd);
         }
 
         public void AdmitPatient(Guid patientId, DateTimeOffset when)
         {
+            var cmd = new AdmitPatient()
+                          {
+                              CommandId = Guid.NewGuid(),
+                              PatientId = patientId,
+                              When = when
+                          };
+            _bus.Send(cmd);
         }
 
         public void AssignBed(Guid patientId, int bed)
         {
+            var cmd = new AssignBed()
+                          {
+                              CommandId = Guid.NewGuid(),
+                              PatientId = patientId,
+                              Bed = bed
+                          };
+            _bus.Send(cmd);
         }
 
         public void DischargePatient(Guid patientId, DateTimeOffset when)
         {
+            var cmd = new DischargePatient()
+                          {
+                              CommandId = Guid.NewGuid(),
+                              PatientId = patientId,
+                              When = when
+                          };
+            _bus.Send(cmd);
         }
     }
 }
