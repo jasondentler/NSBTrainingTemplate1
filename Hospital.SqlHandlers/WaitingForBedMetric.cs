4763ec8ecf72ff01d1da1d70b3d9a93cda1b0a44
Jason Dentler <jasondentler@gmail.com>
2012-02-17 07:16:24 -0600

    Added a BAM saga example.

---
+++ Hospital.SqlHandlers/WaitingForBedMetric.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Hospital.Events;
+using NServiceBus;
+using NServiceBus.Saga;
+
+namespace Hospital.SqlHandlers
+{
+    public class WaitingForBedMetric : 
+        Saga<WaitingForBedMetricState>,
+        IAmStartedByMessages<PatientAdmitted>,
+        IHandleMessages<BedAssigned>,
+        IHandleMessages<PatientDischarged>
+    {
+        private readonly IBus _bus;
+
+        public WaitingForBedMetric(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public override void ConfigureHowToFindSaga()
+        {
+            ConfigureMapping<PatientAdmitted>(s => s.PatientId, e => e.PatientId);
+            ConfigureMapping<BedAssigned>(s => s.PatientId, e => e.PatientId);
+            ConfigureMapping<PatientDischarged>(s => s.PatientId, e => e.PatientId);
+        }
+
+        public void Handle(PatientAdmitted message)
+        {
+            Data.PatientId = message.PatientId;
+            RequestUtcTimeout<object>(TimeSpan.FromHours(1));
+        }
+
+        public void Handle(BedAssigned message)
+        {
+            MarkAsComplete();
+        }
+
+        public void Handle(PatientDischarged message)
+        {
+            MarkAsComplete();
+        }
+
+        public override void Timeout(object state)
+        {
+            _bus.Send(new WaitingForBedMetricExceeded()
+                          {
+                              PatientId = Data.PatientId
+                          });
+        }
+    }
+
+    public class WaitingForBedMetricState : IContainSagaData
+    {
+
+        public Guid Id { get; set; }
+        public string Originator { get; set; }
+        public string OriginalMessageId { get; set; }
+
+        public Guid PatientId { get; set; }
+
+    }
+
+    public class WaitingForBedMetricExceeded : Hospital.IMessage
+    {
+
+        public Guid PatientId { get; set; }
+
+    }
+
+}
